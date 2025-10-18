3d1b651f76de6465acd9a3c6d38b4783198cf8e8
Aditya Jadhav <aditya.jadhav@browza.in>
2025-10-19 00:00:41 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Inter, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from 'react-hot-toast';
-import StoreProvider from "./storeProvider";
-
 const geistSans = Inter({ subsets: ["latin"], variable: "--font-geist-sans" });
 const geistMono = Roboto_Mono({ subsets: ["latin"], variable: "--font-geist-mono" });
 
@@ -23,11 +21,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <StoreProvider>
-
              {children}  
           
-          </StoreProvider>  
            <Toaster /> 
       </body>
     </html>
