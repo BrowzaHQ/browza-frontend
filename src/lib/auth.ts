3c47c4107dbe8c54fe789a5073bb1c11dc41792d
Aditya Jadhav <aditya.jadhav@browza.in>
2025-10-22 23:39:59 +0530

    Create auth.ts

---
+++ src/lib/auth.ts
@@ -0,0 +1,5 @@
+let _token: string | null = null;
+export const setAuthToken = (t: string | null) => { _token = t; };
+export const getAuthToken = () => _token;
+export const clearAuth = () => { _token = null; };
+export const isAuthed = () => !!_token;
